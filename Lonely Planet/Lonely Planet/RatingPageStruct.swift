bd9d4562b754341273fbb255f8c0a4ff1fc42d9b
nickwu123 <wuchanghao2009@163.com>
2020-02-18 09:05:55 +0800

    Init code

---
+++ Lonely Planet/Lonely Planet/RatingPageStruct.swift
@@ -0,0 +1,13 @@
+//
+//  RatingPageStruct.swift
+//  Lonely Planet
+//
+//  Created by Nick.Wu on 2020/2/5.
+//  Copyright © 2020 Suzy.Wang. All rights reserved.
+//
+
+import Foundation
+
+struct RatingPageStruct {
+    let starImage = ["Broring", "Meh"]
+}
